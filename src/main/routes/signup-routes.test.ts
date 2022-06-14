690abe1c73a95347628614ff13746e187f1e87c0
JoaoLeonello <jfelipe.pl@gmail.com>
2022-06-13 21:34:51 -0300

    test: ensure mongodb reconnect if client is down

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -11,6 +11,11 @@ describe('Signup Routes', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
